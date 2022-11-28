20219cf68fac8595aa796186304528d30ccebb1e
PavloDd <86347147+PavloDd@users.noreply.github.com>
2022-11-28 13:48:24 +0200

    foiwfw

--- laboratorna1.cpp
+++ laboratorna1.cpp
@@ -6,6 +6,6 @@ int main()
     double y = 3.981;
     double task = log10(pow(x, 2) + 4 * x * y + pow(y, 2)) - 12 * cos(x * pow(y, 4)) + (13 * pow(x, 6));
     printf("%lf", task);
-
+    //changing
 }
 
